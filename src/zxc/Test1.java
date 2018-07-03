6c14ebf6feba9ef982dfdc34f878a28389bc0a05
Saber-Nero <xuzhen0xz@163.com>
2018-07-03 11:00:23 +0800

    shanchu

--- src/zxc/Test1.java
+++ src/zxc/Test1.java
@@ -4,7 +4,6 @@ public class Test1 {
   public static void main(String[] args) {
 	System.out.println(111);
 	System.out.println("dfgaerhsrtjfdsds");
-	System.out.println("gengxin");
 	System.out.println("1111111116548");
 }
 } 
