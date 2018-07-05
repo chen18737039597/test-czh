43d1fe520d23e655239e26fc302a6215b7ef1ea1
chen18737039597 <18737039597@163.com>
2018-07-05 16:03:28 +0800

    今天第二次

--- src/zxc/Test1.java
+++ src/zxc/Test1.java
@@ -1,11 +1,9 @@
 package zxc;
 
 public class Test1 {
-  public static void main(String[] args) {
-	System.out.println(111);
-	System.out.println("dfgaerhsrtjfdsds");
-	System.out.println("1111111116548");
-	//System.out.println("ffffffffffff");
-	System.out.println();
-}
+
+    public static void main(String[] args) {
+
+        System.out.printf(111);
+    }
 } 
